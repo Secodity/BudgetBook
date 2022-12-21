6f5e735292da64a507735e32d00029208b7b4458
Marvin Secodity Mees <118847092+Secodity@users.noreply.github.com>
2022-12-21 08:48:13 +0100

    Added .editorconfig and adjusted the variable names of RegularTransactionTest to confirm with the .editorconfig

--- BudgetBook.Test/Entities/RegularTransactionTest.cs
+++ BudgetBook.Test/Entities/RegularTransactionTest.cs
@@ -14,53 +14,53 @@ namespace BudgetBook.Test.Entities
         [TestMethod]
         public void TestOfRegularTransaction_Month()
         {
-            var Transaction = new RegularTransaction()
+            var transaction = new RegularTransaction()
             {
                 Amount = 13,
                 InitDate = new DateOnly(2022, 12, 07),
                 Frequency = Backend.eFrequency.Weekly,
             };
-            var NextDuty = Transaction.GetNextDuty(DateOnly.FromDateTime(DateTime.Now));
-            Assert.AreEqual(new DateOnly(2022, 12, 21), NextDuty);
+            var nextDuty = transaction.GetNextDuty(DateOnly.FromDateTime(DateTime.Now));
+            Assert.AreEqual(new DateOnly(2022, 12, 21), nextDuty);
         }
 
         [TestMethod]
         public void TestOfRegularTransaction_Month_InFuture()
         {
-            var Transaction = new RegularTransaction()
+            var transaction = new RegularTransaction()
             {
                 Amount = 13,
                 InitDate = new DateOnly(2099, 05, 06),
                 Frequency = Backend.eFrequency.Weekly,
             };
-            var NextDuty = Transaction.GetNextDuty(DateOnly.FromDateTime(DateTime.Now));
-            Assert.AreEqual(DateOnly.FromDateTime(DateTime.Now), NextDuty);
+            var nextDuty = transaction.GetNextDuty(DateOnly.FromDateTime(DateTime.Now));
+            Assert.AreEqual(DateOnly.FromDateTime(DateTime.Now), nextDuty);
         }
 
         [TestMethod]
         public void TestOfRegularTransaction_Month_Today()
         {
-            var Transaction = new RegularTransaction()
+            var transaction = new RegularTransaction()
             {
                 Amount = 13,
                 InitDate = DateOnly.FromDateTime(DateTime.Now),
                 Frequency = Backend.eFrequency.Weekly,
             };
-            var NextDuty = Transaction.GetNextDuty(DateOnly.FromDateTime(DateTime.Now));
-            Assert.AreEqual(DateOnly.FromDateTime(DateTime.Now), NextDuty);
+            var nextDuty = transaction.GetNextDuty(DateOnly.FromDateTime(DateTime.Now));
+            Assert.AreEqual(DateOnly.FromDateTime(DateTime.Now), nextDuty);
         }
 
         [TestMethod]
         public void TestOfRegularTransaction_Month_Factor3()
         {
-            var Transaction = new RegularTransaction()
+            var transaction = new RegularTransaction()
             {
                 Amount = 13,
                 InitDate = new DateOnly(2022, 12, 13),
                 Frequency = Backend.eFrequency.Weekly,
             };
-            var NextDuty = Transaction.GetNextDuty(3, DateOnly.FromDateTime(DateTime.Now));
-            Assert.AreEqual(new DateOnly(2023, 01, 03), NextDuty);
+            var nextDuty = transaction.GetNextDuty(3, DateOnly.FromDateTime(DateTime.Now));
+            Assert.AreEqual(new DateOnly(2023, 01, 03), nextDuty);
         }
 
     }
